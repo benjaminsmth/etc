4250588649f006d0fed829e9915f2d8ef7b34c8b
bjnsmith <bjnsmith@gmail.com>
2016-06-08 18:02:17 -0500

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int f[6]; /* Declare array to store fibonacci sequence */
+	f[0] = 0; 
+	f[1] = 1; // Set seed values for fibonacci sequence.
+	for ( int i=2; i < 6; ++i)
+	{
+	f[i] = f[i-1] + f[i-2];
+
+	}
+	for ( int k = 0; k < 6; ++k)
+	{
+	    printf("%d\n", f[k]);
+	}
+}
